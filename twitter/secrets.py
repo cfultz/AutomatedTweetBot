688afb3aa10b02c6137e17712ec70db4b2f26290
Caleb Fultz <calebfultz@gmail.com>
2018-10-28 09:28:45 -0400

    Create secrets.py

---
+++ twitter/secrets.py
@@ -0,0 +1,6 @@
+### secure way to deliver the goods
+### apply for your dev account and fill in with your creds
+consumer_key = 'CONSUMER_KEY'
+consumer_secret = 'CONSUMER_SECRET'
+access_token = 'ACCESS_TOKEN'
+access_secret = 'ACCESS_SECRET'
